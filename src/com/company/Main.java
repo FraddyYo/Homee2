4118660c31d0b1e7744547ab01e831c79197534f
Georgii <loveOlechka@mail.ru>
2021-12-28 21:51:22 +0300

    Коммит сбился какой)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,70 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int clientDeviceYear = 2015;
+        if (clientDeviceYear < 2015) ;
+        {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            {
+                if (clientDeviceYear >= 2015) ;
+                {
+                    System.out.println("Приложение будет работать корректно для Android");
+                }
+            }
+        }
+    }
+
+    {
+        byte IOS = 0;
+        if (IOS == 0) ;
+        {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        byte Android = 1;
+        if (Android == 1) ;
+        System.out.println("Установите версию приложения для Android по ссылке");
+    }
+
+    {
+        int year = 2021;
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) ;
+
+        System.out.println(" 2021 год не является високосным");
+        {
+            int deliveryDistance = 95;
+            if (deliveryDistance > 20 && deliveryDistance > 60 && deliveryDistance >= 100) ;
+            System.out.println("Доставка займет 3 дня");
+        }
+    }
+
+    {
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
     }
 }
+
+
