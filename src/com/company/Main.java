a9bc9f801d849d7cf23a9ed4feeef4b757dc1f6d
Georgii <loveOlechka@mail.ru>
2022-01-04 16:18:22 +0300

    сотый раз переделываю ((  Код подправил

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,57 +18,65 @@ public class Main {
             System.out.println("Приложение будет работать корректно для Android");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
-            if (clientDeviceYearIOS <= 2015 && clientIOS == 1) {
-                System.out.println("Приложение будет работать корректно для IOS");
-            } else {
-                System.out.println("Установите версию приложения для IOS по ссылке");
-            }
-            // Задание 3
-            int year = 2021;
-            if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
-                System.out.println(" Год является високосным");
-            } else {
-                if ((year % 400 != 0) || ((year % 4 != 0) && (year % 100 == 0))) ;
-                System.out.println(" 2021 год не является високосным");
-            }
-            // Задание 4
-            int deliveryDistance = 20;
-            int deliveryDays = 1;
-            if (deliveryDistance > 60 && deliveryDistance <= 100) {
-                deliveryDays++;
-            } else {
-                if (deliveryDays++ < 20 && deliveryDays++ < 60 && deliveryDistance <= 100) ;
-                System.out.println(" Потребуется " + deliveryDays++ + " дня");
-            }
-            // Задание 5
-            int monthNumber = 12;
-            switch (monthNumber) {
-                case 1:
-                case 2:
-                case 12:
-                    System.out.println("Зима");
-                    break;
-                case 3:
-                case 4:
-                case 5:
-                    System.out.println("Весна");
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                    System.out.println("Лето");
-                    break;
-                case 9:
-                case 10:
-                case 11:
-                    System.out.println("Осень");
-                    break;
-                default:
-                    System.out.println("Такого месяца не существует");
-            }
+        }
+        if (clientDeviceYearIOS <= 2015 && clientIOS == 1) {
+            System.out.println("Приложение будет работать корректно для IOS");
+        } else {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        }
+
+        // Задание 3
+        int year = 2021;
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+            System.out.println(" Год является високосным");
+        } else {
+            System.out.println("Год не является високосным ");
+        }
+
+        // Задание 4
+        int deliveryDistance = 20;
+        int deliveryDays = 1;
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            deliveryDays++;
+        }
+        if (deliveryDays++ < 20 && deliveryDays++ < 60 && deliveryDistance <= 100) ;
+        System.out.println(" Потребуется " + deliveryDays++ + " дня");
+
+        // Задание 5
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
         }
     }
 }
+
+
+
+
+
+
+
 
 
 
